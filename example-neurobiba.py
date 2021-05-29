630c4568b3fc7941540af5af77ac73ccc8eab685
displaseman <bardocost@gmail.com>
2021-05-29 22:34:39 +0300

    version 0.2

--- example-neurobiba.py
+++ example-neurobiba.py
@@ -1,19 +1,19 @@
 from neurobiba import *
 from random import random 
 
-syn = create_weights([2, 1]) #Создаем веса
+weights = create_weights([2, 1]) #Создаем веса
 
 #Учим нейросеть определять что первое число больше второго
 for i in range(1000): 
-    inp = [random(),random()]
-    syn = training(inp, [int(inp[0]>inp[1])], syn) 
+    input_layer = [random(),random()]
+    weights = training(input_layer, [int(input_layer[0]>input_layer[1])], weights) 
 
 #Тестируем обученную нейросеть
 t = 0
 for i in range(100):
-    inp = [random(),random()]
-    re = round(result(inp, syn)[0])
-    if re == int(inp[0]>inp[1]): t+=1
+    input_layer = [random(),random()]
+    result= round(feed_forward(input_layer, weights)[0])
+    if result== int(input_layer[0]>input_layer[1]): t+=1
 
 print('Нейросеть ответила верно в ', t, ' случаях из 100')
     
--- neurobiba.py
+++ neurobiba.py
@@ -17,20 +17,18 @@ def sigmoid(x):
 """
 Функция create_weights нужна для создания весов. 
 
-В качестве агрумента принимает список размеров слоев нейросети. 
-Возможное слоев от 2 до бесконечности.
+l_size это список слоев с количеством их нейронов.
 
 Пример использования функции:
-syn = create_weights([3,10,10,2])
+weights = create_weights([3,10,10,2])
 Здесь три нейрона на входном слое, 
-два промежуточных слоя по 10 нейронов 
-и 2 нейрона на выходе.
+два промежуточных слоя по 10 нейронов и 2 нейрона на выходе.
 """
 def create_weights(l_size): 
-    syn = []
+    weights = []
     for i in range(len(l_size)-1):
-        syn.append(2*random.random((l_size[i], l_size[i+1])) - 1)
-    return syn
+        weights.append(2*random.random((l_size[i], l_size[i+1])) - 1)
+    return weights
 
 
 """
@@ -41,27 +39,27 @@ def create_weights(l_size):
 
 Пример использования функции:
 for i in range(1000):
-    syn = training(inp, correct_output, syn)
+    weights = training(input_layer, correct_output, weights)
     
-inp это список нейронов входного слоя. 
+input_layer это список нейронов входного слоя. 
 Они должны находится в диапазоне от 0 до 1.
-Например: inp = [1, 0, 0.7]
+Например: input_layer = [1, 0, 0.7]
 
 correct_output это список правильных выходов для заданных входов.
 Они должны находится в диапазоне от 0 до 1.
 Например: correct_output = [0.5, 1]
 
-syn это веса нейросети, которые вы создали ранее
+weights это веса нейросети, которые вы создали ранее
 
 alpha это коэфициент скорости обучения. 
 Его оптимальное значение меняется в зависимости от задачи.
 """
-def training(inp, correct_output, syn, alpha = 0.9):
-    l = [array([inp])]
-    d = len(syn)
+def training(input_layer, correct_output, weights, alpha = 0.9):
+    l = [array([input_layer])]
+    d = len(weights)
     
     for i in range(d):
-        l.append(sigmoid(dot(l[-1],syn[i])))
+        l.append(sigmoid(dot(l[-1],weights[i])))
        
     l_error = []
     l_delta = []
@@ -70,67 +68,67 @@ def training(inp, correct_output, syn, alpha = 0.9):
     l_delta.append(l_error[-1] * sigmoid_derivative(l[-1], alpha) )
 
     for i in range(d-1):
-        l_error.append(l_delta[i].dot(syn[d-1-i].T))
+        l_error.append(l_delta[i].dot(weights[d-1-i].T))
         l_delta.append(l_error[-1] * sigmoid_derivative(l[d-1-i], alpha))
     
-    for ind, i in enumerate(syn):
-        syn[ind] += l[ind].T.dot(l_delta[-1-ind])
+    for ind, i in enumerate(weights):
+        weights[ind] += l[ind].T.dot(l_delta[-1-ind])
         
-    return syn
+    return weights
 
 
 """
-Функция result нужна для получения ответа нейросети.
+Функция feed_forward нужна для получения ответа нейросети.
 Возвращает список выходных нейронов.
 
 Пример использования:
-r = result(inp, syn)
+r = result(input_layer, weights)
 
-inp это список входных нейронов.
+input_layer это список входных нейронов.
 
-syn это веса нейросети, которые вы создали ранее.
+weights это веса нейросети, которые вы создали ранее.
 """
-def result(inp, syn): 
-    l = [array([inp])]
-    d = len(syn)
+def feed_forward(input_layer, weights): 
+    l = [array([input_layer])]
+    d = len(weights)
     
     for i in range(d):
-        l.append(sigmoid(dot(l[-1],syn[i])))
+        l.append(sigmoid(dot(l[-1],weights[i])))
 
     return l[-1][0]
 
 
 """
-Функция reverse работает так же как result,
+Функция feed_reverse работает так же как result,
 но проводит сигнал через нейросеть в обратном направлении.
 В нее в качестве входного слоя подают то что ранее считалось выходным слоем 
 
 
 Пример использования:
-r = reverse(inp, syn)
+r = reverse(input_layer, weights)
 """
-def reverse(inp, syn):
-    synr = list(reversed(syn))
-    for ind, i in enumerate(synr):
-        synr[ind] = synr[ind].T
+def feed_reverse(input_layer, weights):
+    weightsr = list(reversed(weights))
+    for ind, i in enumerate(weightsr):
+        weightsr[ind] = weightsr[ind].T
         
-    l = [array([inp])]
-    d = len(synr)
+    l = [array([input_layer])]
+    d = len(weightsr)
 
     for i in range(d):
-        l.append(sigmoid(dot(l[-1],synr[i])))
+        l.append(sigmoid(dot(l[-1],weightsr[i])))
     return l[-1][0]
 
 
 """
-Функция download_syn нужна для загрузки весов из файла
+Функция download_weights нужна для загрузки весов из файла
 
 Пример использования:
-syn = download_syn()
+weights = download_weights()
 
 В качестве аргумента file_name можно указать имя файла .dat без указания формата.
 """
-def download_syn(file_name = 'syn'):
+def download_weights(file_name = 'weights'):
     try: 
         with open(f'{file_name}.dat','rb') as file:
             return pickle.load(file)
@@ -139,14 +137,16 @@ def download_syn(file_name = 'syn'):
 
 
 """
-Функция save_syn нужна для сохранения весов в файл.
+Функция save_weights нужна для сохранения весов в файл.
 
 Пример использования:
-save_syn(syn)
+save_weights(weights)
 
 В качестве аргумента file_name можно указать имя файла .dat без указания формата.
 """
-def save_syn(syn, file_name = 'syn'):
+def save_weights(weights, file_name = 'weights'):
     with open(f'{file_name}.dat','wb') as file:
-        pickle.dump(syn, file)
+        pickle.dump(weights, file)
     print('file saved')
+
+
