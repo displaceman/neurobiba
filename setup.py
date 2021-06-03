9525b66bf6ada304e1ef2aa181403dbdce0c368e
displaceman <62401233+displaceman@users.noreply.github.com>
2021-06-03 21:27:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,12 @@
 from setuptools import setup
 
-<<<<<<< HEAD
-setup(name='neurobiba',
-      version='0.3',
-      description='small collection of functions for neural networks',
-      packages=['neurobiba'],
-      author_email='cumnaamys@gmail.com',
-      url="https://github.com/displaceman/neurobiba",
-      python_requires=">=3.7.7",
-      zip_safe=False)
-=======
 setup(
     name='neurobiba',
     version='0.4',
     description='small collection of functions for neural networks',
     url='https://github.com/displaceman/neurobiba',
+    author_email='cumnaamys@gmail.com',
     author='displaceman',
     license='GPL',
     packages=['pickle, numpy'],
     zip_safe=False)
->>>>>>> 452d90459d8b73961b0a2bb94e2652c2f5c3126c
