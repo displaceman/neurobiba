21c1398c813429bcdc16e7e62ffac94178a23bfc
displaseman <bardocost@gmail.com>
2021-06-03 21:22:24 +0300

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='neurobiba',
+      version='0.3',
+      description='small collection of functions for neural networks',
+      packages=['neurobiba'],
+      author_email='cumnaamys@gmail.com',
+      url="https://github.com/displaceman/neurobiba",
+      python_requires=">=3.7.7",
+      zip_safe=False)
