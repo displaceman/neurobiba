85f68977bbf21c81b2eeae4e42041f29ee6f93a7
displaceman <62401233+displaceman@users.noreply.github.com>
2021-06-04 14:10:05 +0300

    Update example_00.py

--- examples/example_00.py
+++ examples/example_00.py
@@ -5,14 +5,14 @@ from random import random
 weights = create_weights([2, 1]) 
 
 #Учим нейросеть определять, больше ли первое число второго
-for i in range(1000): 
+for _ in range(1000): 
     first, second = random(), random()
     result = int(first > second)
     weights = training([first, second], [result], weights) 
 
 #Тестируем обученную нейросеть
 correct = 0
-for i in range(100):
+for _ in range(100):
     first, second = random(), random()
     result = round(feed_forward([first, second], weights)[0])
     if result == int(first > second): 
