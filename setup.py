ca5ca8aed1d72f6293bcea9d137e7dd28d79b87e
Yura Frozen <yurafrozen@gmail.com>
2021-06-04 14:21:02 +0500

    upd setup

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(
     name='neurobiba',
     version='0.6',
     description='small collection of functions for neural networks',
+    long_description='small collection of functions for neural networks',
     url='https://github.com/displaceman/neurobiba',
     author_email='cumnaamys@gmail.com',
     author='displaceman',
     license='GPL',
     packages=find_packages(),
     install_requires=['numpy'],
-    zip_safe=False)
+    zip_safe=False
+)
