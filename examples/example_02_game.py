c336e639f53df7743928c6fac0b29d08a4b56dfe
displaseman <bardocost@gmail.com>
2021-06-04 20:43:00 +0300

    Update example_02.py

--- examples/example_02_game.py
+++ examples/example_02_game.py
@@ -20,7 +20,7 @@ dataset = []
 
 active = False
 
-basicFont = pg.font.SysFont(None, 20)
+basicFont = pg.font.SysFont('arial', 15, bold=True)
 def write_on_screen (text, x, y):
     text = basicFont.render(text, True, (255, 0, 0))
     textRect = text.get_rect()
@@ -59,21 +59,21 @@ while True:
             color = tuple([result * 255] * 3)
             screen.fill(color, (x * PX, y * PX, PX, PX))
 
+    screen.fill((255, 0, 0), (4, 4, 22, 22))
+    color = (255, 255, 255) if active else (0, 0, 0)
+    screen.fill(color, (5, 5, 20, 20))
+
+    write_on_screen('MOUSE : set point', 30, 10)
+    write_on_screen('X : switch color', 30, 25)
+    write_on_screen('DEL : clear dataset', 30, 40)
+    write_on_screen('N : reset weights', 30, 55)
+
     for data in dataset:
         color = (255, 255, 255) if data["value"] else (0, 0, 0)
         x = int(data["point"][0] * WIDTH * PX)
         y = int(data["point"][1] * HEIGHT * PX)
         pg.draw.circle(screen, (255, 0, 0), (x, y), 7)
         pg.draw.circle(screen, color, (x, y), 5)
-
-    screen.fill((255, 0, 0), (4, 4, 22, 22))
-    color = (255, 255, 255) if active else (0, 0, 0)
-    screen.fill(color, (5, 5, 20, 20))
-
-    write_on_screen('MOUSE - set point', 30, 10)
-    write_on_screen('X - switch color', 30, 25)
-    write_on_screen('DELETE - clear dataset', 30, 40)
-    write_on_screen('N - reset weights', 30, 55)
 
     pg.display.flip()
 
