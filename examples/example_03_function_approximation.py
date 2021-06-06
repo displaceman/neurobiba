1a094058f534499f0ef345fb03ba3bec35b63e25
displaseman <bardocost@gmail.com>
2021-06-06 22:00:13 +0300

    Update example_03

--- examples/example_03_function_approximation.py
+++ examples/example_03_function_approximation.py
@@ -2,6 +2,7 @@ from neurobiba import *
 
 import random
 import matplotlib.pyplot as plt
+import math
 
 neural_net = Weights([1, 50, 1], bias = True)
 
@@ -11,22 +12,21 @@ result = []
 dimension = 20
 
 def example_function(x):
-    x = x+0.5
-    return (((4*x**3-6*x**2+1)*(x+1)**0.5)/(3-x)+1)*0.5
+    return (math.sin(x*10)+1)*0.5
     
-for i in range(1000):
+for i in range(10000):
     for u in range(dimension):
         u /= dimension
         neural_net.train([u], [example_function(u)])
 
 for u in range(dimension):
     u /= dimension
-    
     x_axis.append(u)
     dataset.append(example_function(u))
     result.append(neural_net.feed_forward([u])[0])
 
-plt.plot(x_axis, result)
-plt.plot(x_axis, dataset)
-plt.title('result')
+plt.title('function approximation')
+plt.plot(x_axis, dataset, label = 'example function')
+plt.plot(x_axis, result, label = 'neural network response')
+plt.legend()
 plt.show()
