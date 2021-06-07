e1a540522a1bc33e3ca2efddc78147b287104367
displaseman <bardocost@gmail.com>
2021-06-07 04:26:21 +0300

    fix

--- neurobiba/__init__.py
+++ neurobiba/__init__.py
@@ -18,10 +18,8 @@ class Weights():
         bias этот флаг добавляет к каждому слою нейрон смещения.
         name это имя файла в который будет сохранять функция save_weights.
         activation это объект класса Activation который содержит в себе функцию активации и ее производную
-
         Пример:
         `weights = Weights([3, 10, 10 ,2])`
-
         Здесь три нейрона на входном слое, 
         два промежуточных слоя по 10 нейронов и 2 нейрона на выходе.
         """
@@ -47,21 +45,17 @@ class Weights():
         методом обратного распространения ошибки.
         Меняет веса.
         Ее нужно вызывать в цикле столько раз сколько потребуется для обучения.
-
         Пример:
         ```
         for i in range(1000):
             weights.training(input_layer, correct_output)
         ```
-
         `input_layer` - это список нейронов входного слоя. 
         Они должны находится в диапазоне от `0` до `1`.
         Например: `input_layer = [1, 0, 0.7]`
-
         `correct_output` - это список правильных выходов для заданных входов.
         Они должны находится в диапазоне от `0` до `1`.
         Например: `correct_output = [0.5, 1]`
-
         `alpha` - это коэфициент скорости обучения. 
         Его оптимальное значение меняется в зависимости от задачи.
         """
@@ -77,7 +71,6 @@ class Weights():
         # Корректрирует остальные слои весов
         for i in range(len(self.weights)-1):
             error = delta.dot(self.weights[len(self.weights)-1-i].T)
-            #delta = error * self.activation.deriv(layers[len(self.weights)-1-i]) * alpha       #Без биаса нижние две строи могли бы выглядеть так
             delta = (error * self.activation.deriv(layers[len(self.weights)-1-i]) * alpha)[0]
             delta = array([delta[:len(delta)-self.bias]])
             self.weights[-2-i] += layers[-3-i].T.dot(delta)
@@ -87,10 +80,8 @@ class Weights():
         """
         Метод для получения ответа нейросети.
         Возвращает список выходных нейронов.
-
         Пример:
         `result = weights.feed_forward(input_layer)`
-
         `input_layer` - это список входных нейронов.
         """
         return self._feed_forward(input_layer)[-1][0]
@@ -102,11 +93,8 @@ class Weights():
         len_weights = len(self.weights)
 
         for i in range(len_weights):
-            if self.bias:
-                layers[-1] = array([append(layers[-1], 1)])
-
-            #layers = self._feed_forward_strategy(layers)
-
+            #if self.bias: layers[-1] = array([append(layers[-1], 1)])    #Альтернатива - следующая строка
+            layers = self._feed_forward_strategy(layers)
             layers.append(self.activation.fn(
                 dot(layers[-1], self.weights[i])))
 
@@ -115,11 +103,9 @@ class Weights():
     def feed_backward(self, input_layer):
         """
         НЕ РАБОТАЕТ НА ВЕСАХ С БИАСОМ
-
         Эта функция работает так же, как `feed_forward`,
         но проводит сигнал через нейросеть в обратном направлении.
         В нее в качестве входного слоя подают то, что ранее считалось выходным слоем.
-
         Пример:
         `r = weights.feed_backward(input_layer)`
         """
@@ -158,10 +144,8 @@ def _feed_backward_with_bias(_weights, _input_layer):
 def load_weights(file_name=_WEIGHTS_NAME_PREFIX + "0") -> Weights:
     """
     Загрузка весов из файла.
-
     Пример использования:
     `weights = load_weights()`
-
     В качестве аргумента `file_name` можно указать имя файла
     """
 
@@ -176,10 +160,8 @@ def load_weights(file_name=_WEIGHTS_NAME_PREFIX + "0") -> Weights:
 def save_weights(weights, file_name=None):
     """
     Схранение весов в файл.
-
     Пример использования:
     `save_weights(weights)`
-
     В качестве аргумента `file_name` можно указать имя файла
     """
     if file_name is None:
