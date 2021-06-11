cfcd85b33398789c474ff21de496c1f4ce4c9a8b
displaceman <62401233+displaceman@users.noreply.github.com>
2021-06-11 14:00:50 +0300

    Update activations.py

--- neurobiba/activations.py
+++ neurobiba/activations.py
@@ -17,10 +17,13 @@ class Activation():
 
 def _sigmoid(x):
     return 1 / (1 + exp(-x))
-
-
 def _sigmoid_derivative(x):
     return x * (1 - x)
-
-
 SIGMOID = Activation(_sigmoid, _sigmoid_derivative)
+
+
+def _relu(x):
+    return (x>0)*x
+def _relu_derivative(x):
+    return (x>=0)*1
+RELU = Activation(_relu, _relu_derivative)
